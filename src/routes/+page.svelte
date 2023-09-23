b5164f07fb6b48850d897cbb484fdb80f94636b4
muje hoxe <mujehoxe@gmail.com>
2023-09-23 20:39:58 +0100

    dashboard init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,15 @@
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
+<script>
+	import Dashboard from './Dashboard.svelte';
+</script>
+
+<div class="md:pl-64 flex flex-col flex-1">
+	<main class="flex-1">
+		<Dashboard />
+	</main>
+</div>
 
 <style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
-  }
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
 </style>
