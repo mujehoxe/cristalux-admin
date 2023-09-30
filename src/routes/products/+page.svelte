f460f3bcb05e367bc0461a9ad7cfbf1538c098dd
muje hoxe <mujehoxe@gmail.com>
2023-09-30 08:20:55 +0100

    products pagination

--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -1,11 +1,25 @@
 <script>
+	import CreateProduct from './CreateProduct.svelte';
+	import Pagination from './Pagination.svelte';
+
 	import ProductCard from './ProductCard.svelte';
 
 	/** @type {import('./$types').PageServerLoad} */
 	export let data;
-	/** @type {[]}*/
 	let { products } = data;
-	let [pageProducts, totalProductsLenght] = products;
+	let [pageProducts, totalRows] = products;
+	let perPage = pageProducts.length;
+	let currentPage = 0;
+
+	export async function loadPage() {
+		const res = await fetch(
+			`http://localhost:3000/api/v1/products?page=${
+				currentPage + 1
+			}&pageSize=10`
+		);
+		[pageProducts, totalRows] = await res.json();
+		return { products };
+	}
 </script>
 
 {#if pageProducts !== undefined}
@@ -14,6 +28,9 @@
 			Products
 		</h2>
 		<CreateProduct />
+		<div class="p-6">
+			<Pagination {totalRows} {perPage} bind:currentPage {loadPage} />
+		</div>
 		<div class="flex flex-wrap justify-evenly justify-items-start gap-4 mt-4">
 			{#key pageProducts}
 				{#each pageProducts as product}
---
+++ src/routes/products/Pagination.svelte
@@ -0,0 +1,89 @@
+<script>
+	import Fa from 'svelte-fa';
+	import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+
+	export let totalRows = 0;
+	export let perPage = 0;
+	export let currentPage = 1;
+
+	/** @type {() => Promise<{ products: any; }>} */
+	export let loadPage;
+
+	$: totalPages = Math.ceil(totalRows / perPage);
+	$: currentPage;
+
+	function isPageSelected(page) {
+		return page === currentPage;
+	}
+</script>
+
+{#key currentPage}
+	{#if totalRows && totalRows > perPage}
+		<div class="flex justify-center items-center">
+			<button
+				on:click={() => {
+					currentPage -= 1;
+					loadPage();
+				}}
+				disabled={currentPage === 0 ? true : false}
+				aria-label="left arrow icon"
+				aria-describedby="prev"
+			>
+				<Fa icon={faArrowLeft} />
+			</button>
+			<span id="prev" class="sr-only">Load previous {perPage} rows</span>
+			{#each Array(totalPages) as _, index (index)}
+				<button
+					class={`px-2 rounded-full ${
+						isPageSelected(index) && 'bg-indigo-700'
+					}`}
+					on:click={() => {
+						currentPage = index;
+						loadPage();
+					}}
+					disabled={isPageSelected(index) ? true : false}
+					aria-label={`page number ${index}`}
+					aria-describedby="next"
+				>
+					<p>{index + 1}</p>
+				</button>
+			{/each}
+			<button
+				on:click={() => {
+					currentPage += 1;
+					loadPage();
+				}}
+				disabled={currentPage === totalPages - 1 ? true : false}
+				aria-label="right arrow icon"
+				aria-describedby="next"
+			>
+				<Fa icon={faArrowRight} />
+			</button>
+			<span id="next" class="sr-only">Load next {perPage} rows</span>
+		</div>
+	{/if}
+{/key}
+
+<style>
+	.sr-only {
+		position: absolute;
+		clip: rect(1px, 1px, 1px, 1px);
+		padding: 0;
+		border: 0;
+		height: 1px;
+		width: 1px;
+		overflow: hidden;
+	}
+
+	.pagination p {
+		margin: 0 1rem;
+	}
+
+	.selected {
+		background-color: var(--accent-color);
+	}
+
+	button {
+		display: flex;
+	}
+</style>
