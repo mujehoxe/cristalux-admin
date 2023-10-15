133e6884c63a7bc5d4f085d0c78822726e700d4d
muje hoxe <mujehoxe@gmail.com>
2023-10-15 18:00:20 +0100

    hooked with https://cristalux.store

--- src/routes/orders/+page.server.js
+++ src/routes/orders/+page.server.js
@@ -1,6 +1,6 @@
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ fetch }) {
-	const res = await fetch(`api/v1/orders`);
+	const res = await fetch(`https://cristalux.store/api/v1/orders`);
 	const orders = await res.json();
 	return { orders };
 }
