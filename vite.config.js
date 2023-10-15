ec8e66b68ba714da87d8fca0c49766f13f7cc49f
muje hoxe <mujehoxe@gmail.com>
2023-10-15 17:20:50 +0100

    proxy to 3000

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,21 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	server: {
+		proxy: {
+			'/api': {
+				target: 'http://localhost:3000',
+				changeOrigin: true,
+				secure: false,
+				rewrite: (path) => path.replace(/^\/api/, 'api/')
+			},
+			'/uploads': {
+				target: 'http://localhost:3000',
+				changeOrigin: true,
+				secure: false,
+				rewrite: (path) => path.replace(/^\/uploads/, 'uploads/')
+			}
+		}
 	}
 });
