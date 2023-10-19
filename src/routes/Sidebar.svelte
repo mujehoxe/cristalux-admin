6b22b173d3523c3b1579a2ffd090d60eba5f74a6
muje hoxe <mujehoxe@gmail.com>
2023-10-19 04:32:08 +0100

    Sidebar Logo

--- src/routes/Sidebar.svelte
+++ src/routes/Sidebar.svelte
@@ -5,12 +5,11 @@
 <aside class="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
 	<div class="flex-1 flex flex-col min-h-0 bg-gray-800">
 		<div class="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
-			<div class="flex items-center flex-shrink-0 px-4">
-				<img
-					class="h-8 w-auto"
-					src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
-					alt="Workflow"
-				/>
+			<div class="flex items-center gap-4 flex-shrink-0 px-4">
+				<img class="h-8 w-auto" src="/favicon.png" alt="Workflow" />
+				<span class="uppercase text-[#fcf38b] font-bold text-2xl"
+					>cristalux</span
+				>
 			</div>
 			<nav class="mt-5 flex flex-col px-2 space-y-1 gap-2">
 				{#each ['Products', 'Orders'] as name}
